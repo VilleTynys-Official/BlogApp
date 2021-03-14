e6674ba3b94217bad138ba781aa700a46f03ad39
VilleTynysCloubi <ville.tynys@cloubi.com>
2021-03-14 18:42:28 +0200

    Initialize frontend with npx create-react-app

---
+++ src/frontend/src/components/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HomePage = () => {
+  return (
+    <div className='HomePage-container'>
+        <p>helloouuuuu</p>
+    </div>
+  );
+};
+
+export default HomePage;
